cd39e84adf179aeb80b053740bec8af7ac6b24a5
Hermacle <kihenyeghohermacle@gmail.com>
2023-08-14 15:32:55 +0100

    Get HTML elements

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 import { calculate } from './calculator.js';
 // TODO: Faire la manipulation du DOM dans ce fichier
+
+const input = document.getElementById('input');
+const buttons = document.querySelectorAll('button');
+const lastResult = document.querySelector('p');
+ 
